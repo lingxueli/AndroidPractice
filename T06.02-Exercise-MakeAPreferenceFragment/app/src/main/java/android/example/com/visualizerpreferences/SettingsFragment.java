40fdcb28a5797e7afb90b12f00cbe633a77ba02c
lingxueli <ll2871@columbia.edu>
2019-01-13 16:25:17 -0700

    Visualizer app: Add preference fragment

---
+++ T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -0,0 +1,13 @@
+package android.example.com.visualizerpreferences;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        addPreferencesFromResource(R.xml.pref_visualizer);
+    }
+
+}
